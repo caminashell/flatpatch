bf75b286ef46912d0e01eee26db7ad14a6c0ea79
C Λ M I N Λ S H Ξ L L <92443358+caminashell@users.noreply.github.com>
2026-01-07 14:29:05 +0000

    update: find and patch exact file, not symlink file

--- flatpatch.sh
+++ flatpatch.sh
@@ -27,61 +27,90 @@
 # It will require some configuration to process your links properly - see below.
 # It is not only for flatpak applications, but any application that uses a desktop file to launch.
 
-apps=(
-  "loc=./test;app=com.vivaldi.Vivaldi;str=--enable-features=VaapiVideoDecoder,VaapiIgnoreDriverChecks,Vulkan,DefaultANGLEVulkan,VulkanFromANGLE"
-  "loc=./test;app=dev.vencord.Vesktop;str=--user-agent-os=windows"
+# (Flatpak) Applications to patch:
+APPS=(
+  "APP=com.vivaldi.Vivaldi;PATCH=--enable-features=VaapiVideoDecoder,VaapiIgnoreDriverChecks,Vulkan,DefaultANGLEVulkan,VulkanFromANGLE"
+  "APP=dev.vencord.Vesktop;PATCH=--user-agent-os=windows"
 )
-
-# Location(loc): Change this to the location of the desktop files
-# E.g. "/var/lib/flatpak/exports/share/applications"
+# Vivaldi patch: Enable Vivaldi features to play HEVC video (helps with Plex server not needing to transcode)
+# Discord patch: Forced user-agent-os to windows to fix Discord API issues and CDN lockout
 
 # Application(app): Change this to the name of the application
+#                   Do not include the ".desktop" extension
 # E.g. "com.vivaldi.Vivaldi"
 
 # String(str): Change this to the string to check for
-# E.g. " --enable-features=VaapiVideoDecoder,VaapiIgnoreDriverChecks,Vulkan,DefaultANGLEVulkan,VulkanFromANGLE"
+#              It is not necessary to prefix the string with a space(r)
+# E.g. "--enable-features=VaapiVideoDecoder,VaapiIgnoreDriverChecks,Vulkan,DefaultANGLEVulkan,VulkanFromANGLE"
 
-# Vivaldi patch: Enable Vivaldi features to play HEVC video (helps with Plex server not needing to transcode)
-# Discord patch: Forced user-agent-os to windows to fix Discord API issues and CDN lockout
+################################################################################
+
+# Directory to search (change this to the directory you want)
+DIRECTORY="/var/lib/flatpak/app"
+
+# Note: OS uses links stored in the following directory (below).
+#       Each link is a symlink to the applications respective desktop file.
+#       Previously, patching these would rewrite the file and break the icon.
+#       "/var/lib/flatpak/exports/share/applications"
 
 echo "-----"
 
 # Loop through the array and parse each object string
-for app in "${apps[@]}"; do
+for APP in "${APPS[@]}"; do
 
   # Split the string into key-value pairs
-  IFS=';' read -ra key_value_pairs <<< "$app"
+  IFS=';' read -ra key_value_pairs <<< "$APP"
 
   # Loop through the key-value pairs
-  for pair in "${key_value_pairs[@]}"; do
-    IFS='=' read -r key value <<< "$pair"
-    eval $pair
+  for PAIR in "${key_value_pairs[@]}"; do
+    IFS='=' read -r key value <<< "$PAIR"
+    eval $PAIR
   done
 
   # Print process information for logging
-  echo "Application:" $app
-  echo "Location:" $loc
-  echo "String:" $str
+  echo -e "\nApplication:" $APP
+  echo -e "String:" $PATCH"\n"
 
-  # Search for the text in the file
-  grep -qe $str $loc/$app.desktop
+  # File pattern or extension to search for
+  FILE_PATTERN="$APP.desktop"
 
-  # Check the exit status of grep
-  if [ $? -eq 0 ]; then
+  # Find all files in the directory (you can change the file type with -name or -type)
+  find "$DIRECTORY" -type f -name "$FILE_PATTERN" | while read -r FILE; do
 
-    # Print true statement
-    echo -e "\n$app is already patched."
-  else
+    if [[ "$FILE" == *"/export/share/"* ]]; then
 
-    # Print false statement
-    echo -e "\nPatched $app"
+      # Check if the file is readable and writable
+      if [[ -r "$FILE" && -w "$FILE" ]]; then
+        echo "Processing file: $FILE"
 
-    # Find and add the patch string to all locations of the file
-    sed -i "s/\($app\)/\1 $str/" $loc/$app.desktop
-  fi
+        # Search for the text in the file
+        grep -qe " $PATCH" $FILE
 
+        # Check the exit status of grep
+        if [ $? -eq 0 ]; then
+
+          # Print true statement
+          echo -e "\n[✓] The above file is already patched.\n"
+        else
+
+          # Print false statement
+          echo -e "\n[+] Patched the above file.\n"
+
+          # Find and add the patch string to all locations of the file
+          sed -i "s/\( $APP\)/\1 $PATCH/" $FILE
+        fi
+
+      else
+        echo -e "[!] Skipping file (not readable/writable): $FILE\n"
+      fi
+    fi
+
+  done
+
+  # echo "Application patch complete."
   echo "-----"
+
 done
 
 # Print end statement
-echo "Done"
+echo -e "\nProcess complete."
